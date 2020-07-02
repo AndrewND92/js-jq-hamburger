320df901dc301762abb4c5469413a4d41abadd7c
Andrea <miranda.andrea.nd@gmail.com>
2020-07-02 16:41:06 +0200

    finito

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-$('i.fas').click(function(){
+$('i.fa-bars').click(function(){
+
   $('div.hamburger-menu').show();
 
+})
+
+$('i.fa-times').click(function(){
+
+  $('div.hamburger-menu').hide();
 })
